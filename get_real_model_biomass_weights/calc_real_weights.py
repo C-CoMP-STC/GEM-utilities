889e97913617d5c6485a2d3665ec1d8683cd3acb
Helen G Scott <helengracescott@gmail.com>
2025-08-27 17:52:59 -0400

    Gen biomass table for Amac model dev branch

--- get_real_model_biomass_weights/calc_real_weights.py
+++ get_real_model_biomass_weights/calc_real_weights.py
@@ -72,42 +72,20 @@ save_biomass_composition_work_table(
     out_dir=OUT_DIR,
 )
 
-# # Human GEM
-# human_gem_url = "https://raw.githubusercontent.com/SysBioChalmers/Human-GEM/main/model/Human-GEM.xml"
-# print(f"Downloading Human-GEM model from {human_gem_url}...")
-# try:
-#     response = requests.get(human_gem_url)
-#     response.raise_for_status()  # Raise an exception for HTTP errors (4xx or 5xx)
-
-#     # Load the model directly from the string content
-#     # cobra.io.read_sbml_model can take a file path or a file-like object
-#     # For XML text content, use io.StringIO
-#     model_content_str = response.text
-#     human_gem_model = cobra.io.read_sbml_model(io.StringIO(model_content_str))
-#     print("Human-GEM model loaded successfully.")
-
-#     # Now you can use human_gem_model, for example:
-#     weight_human = calculate_biomass_weight(
-#         human_gem_model,
-#         "MAR13082",  # "Generic human cell biomass reaction"
-#         lumped_biomass_components=[
-#             "MAM01721n",  # DNA
-#             "MAM02847c",  # RNA
-#             "MAM10012c",  # cofactor_pool_biomass
-#             "MAM10013c",  # protein_pool_biomass
-#             "MAM10014c",  # lipid_pool_biomass
-#             "MAM10015c",  # metabolite_pool_biomass
-#         ],
-#         save_work_table=True,
-#         out_dir=OUT_DIR,
-#     )
-
-# except requests.exceptions.RequestException as e:
-#     print(f"Error downloading Human-GEM model: {e}")
-# except Exception as e:
-#     print(f"Error processing Human-GEM model: {e}")
-
-
-# # Model with KBase default Gram Negative biomass
-
-# # My current alteromonas model
+# My current alteromonas model
+alteromonas_model = cobra.io.read_sbml_model(
+    "/Users/helenscott/Documents/PhD/Segre-lab/GEM-repos/GEM-mit1002/model.xml"
+)
+save_biomass_composition_work_table(
+    alteromonas_model,
+    "bio1_biomass",
+    mets_to_ignore=[
+        "cpd11416_c0",  # Biomass_c0
+    ],
+    lumped_biomass_components=[
+        "cpd11461_c0",  # DNA [c0]
+        "cpd11463_c0",  # Protein [c0]
+        "cpd11462_c0",  # mRNA [c0]
+    ],
+    out_dir=OUT_DIR,
+)
